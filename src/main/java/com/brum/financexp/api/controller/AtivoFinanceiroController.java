1e2a4aba74d7206504bdd2eda2214f9c7fd90428
lucas.brum <lucas.brum.ext@ipiranga.ipiranga>
2020-04-06 13:42:01 -0300

    Criando método excluir ativo

--- src/main/java/com/brum/financexp/api/controller/AtivoFinanceiroController.java
+++ src/main/java/com/brum/financexp/api/controller/AtivoFinanceiroController.java
@@ -14,12 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brum.financexp.api.model.AtivoFinanceiro;
@@ -85,6 +87,13 @@ public class AtivoFinanceiroController {
 	@PutMapping("/google-sheets")
 	public void atualizarAtivosViaGoogleSheets() {
 		
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@CrossOrigin(origins = "http://localhost:4200")
+	public void remover(@PathVariable Long id) {
+		ativoFinanceiroRepository.deleteById(id);
 	}
 	
 	
