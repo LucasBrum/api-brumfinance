6197616f7a7d46d6b67b51084a160c246cc6b7b3
Lucas Brum <brum.lucas@icloud.com>
2020-04-26 22:07:41 -0300

    Ïmplementando exclusao de aporte financeiro

--- src/main/java/com/brum/financexp/api/controller/AporteController.java
+++ src/main/java/com/brum/financexp/api/controller/AporteController.java
@@ -14,10 +14,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brum.financexp.api.model.Aporte;
@@ -81,6 +84,16 @@ public class AporteController {
 		log.info("Listando Aportes.");
 		
 		return aporteRepository.findAll(pageable);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@CrossOrigin(origins = "http://localhost:4200")
+	public void excluir(@PathVariable Long id) {
+		aporteRepository.deleteById(id);
+		
+		log.info("Aporte excluído com sucesso.");
+
 	}
 	
 	
