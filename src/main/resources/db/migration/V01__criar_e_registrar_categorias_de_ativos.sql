7c61309dae1a087526b4d9658e0819963dfc203a
lucasbrum <brum.lucas@icloud.com>
2022-12-17 12:26:00 -0300

    Mudando nome do sql

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias_de_ativos.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias_de_ativos.sql
@@ -1,4 +1,4 @@
-CREATE TABLE categoria_ativo ( 
+CREATE TABLE categoria_ativo (
 	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	nome varchar(50) NOT NULL,
 	tipo varchar(25) NOT NULL
@@ -7,6 +7,5 @@ CREATE TABLE categoria_ativo (
 INSERT INTO categoria_ativo(nome, tipo) VALUES ('Ações', 'Renda Variável');
 INSERT INTO categoria_ativo(nome, tipo) VALUES ('FIIs', 'Renda Variável');
 INSERT INTO categoria_ativo(nome, tipo) VALUES ('Tesouro Direto', 'Renda Fixa');
-INSERT INTO categoria_ativo(nome, tipo) VALUES ('ETF', 'Renda Variável');
-INSERT INTO categoria_ativo(nome, tipo) VALUES ('Renda Fixa', 'Renda Fixa');
 INSERT INTO categoria_ativo(nome, tipo) VALUES ('Bitcoin', 'Renda Variável');
+INSERT INTO categoria_ativo(nome, tipo) VALUES ('ETF Exterior', 'Renda Variável');
